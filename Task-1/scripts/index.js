c5601c0217c4c48682eec4ce9536db417badd06a
Pankaj_Mehra <meenakshi9991833@gmail.com>
2023-02-03 08:46:17 +0530

    custom validation

--- Task-1/scripts/index.js
+++ Task-1/scripts/index.js
@@ -1,14 +1,41 @@
 class ObjCreator {
-  constructor(e, n) {
-    (this.email = e), (this.number = n);
+  constructor(e, n, c) {
+    (this.email = e), (this.number = n), (this.code = c.s.dialCode);
   }
 }
+
+const numElement = document.getElementById("number");
+const code = window.intlTelInput(numElement, {
+  utilsScript:
+    "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+});
 
 setUserData = (event) => {
   event.preventDefault();
   let email = document.getElementById("email").value;
   let number = document.getElementById("number").value;
-  let userObj = new ObjCreator(email, number);
-  localStorage.setItem("userObj", JSON.stringify(userObj));
-  window.location.href = "./thankYou.html";
+  var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+  if (email == "") {
+    document.getElementById("emailError").innerHTML = "*Required field";
+  } else if (number == "") {
+    document.getElementById("numberError").innerHTML = "*Required field";
+  } else if (!emailPattern.test(email)) {
+    document.getElementById("emailError").innerHTML =
+      "You have entered an invalid email address";
+  } else if (number.length != 10) {
+    document.getElementById("numberError").innerHTML =
+      "You have entered an invalid Mobile number";
+  } else {
+    let userObj = new ObjCreator(email, number, code);
+    localStorage.setItem("userObj", JSON.stringify(userObj));
+    window.location.href = "./thankYou.html";
+  }
+};
+
+const validateNumber = () => {
+  document.getElementById("numberError").innerHTML = "";
+};
+const validateEmail = () => {
+  document.getElementById("emailError").innerHTML = "";
 };
--- Task-1/scripts/thankYou.js
+++ Task-1/scripts/thankYou.js
@@ -1,4 +1,6 @@
 let data = JSON.parse(localStorage.getItem("userObj")) || null;
+console.log('data:', data)
 
 document.getElementById("email").innerText = data.email
+document.getElementById("country-code").innerText += data.code
 document.getElementById("number").innerText = data.number
